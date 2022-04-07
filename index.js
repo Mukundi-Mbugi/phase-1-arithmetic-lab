6041f464b46626d8486c65126cd70c213edc1131
Elijah Mbugi <mukundielijah@gmail.com>
2022-04-07 12:11:09 +0300

    Arithmetic lab test

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1=8.9
+let num2=7
+let multiply= Math.floor(num1*num2)
+
 
 // Write your code here
+let random=Math.floor(Math.random()*10)+1;
+
+let num3=19
+let num4=5
+let mod=num3%num4
+
+
+let max=Math.max(13,20,3,9,17);
